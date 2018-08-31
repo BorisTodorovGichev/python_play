1af36427ccac69d381bf1f7cea6ae7855ea62216
boris.gichev <boris.gichev@gmail.com>
2018-08-31 22:44:53 +0300

    PP-2 added pages for pets and pet detail

--- python_play/python_play/urls.py
+++ python_play/python_play/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+
+from adoptions import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'admin/', admin.site.urls),
+    url(r'^$', views.home, name="home"),
+    url(r"^addoptions/(\d+)/", views.pet_detail, name="pet_detail"),
+
 ]
